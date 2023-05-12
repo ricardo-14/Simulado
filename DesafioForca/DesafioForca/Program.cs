7e13d34274eafb6106bce1961d2d1ecc5e739bee
Ricardo <ricardo.amorimsf@gmail.com>
2023-05-12 17:31:55 -0300

    Primeiro commit

---
+++ DesafioForca/DesafioForca/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioForca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra = "CARRO";
+
+            char[] palavraEscondida = new char[palavra.Length];
+
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                palavraEscondida[i] = '_';
+            }
+
+            while (Convert.ToString(palavraEscondida) != palavra)
+            {
+                Console.WriteLine("Digite uma letra: ");
+                char letra = char.Parse(Console.ReadLine());
+
+                bool letraCerta = false;
+
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    if (palavra[i] == letra)
+                    {
+                        palavraEscondida[i] = letra;
+                        letraCerta = true;
+                    }
+                }
+
+                Console.WriteLine(palavraEscondida);
+
+            }
+
+            Console.WriteLine(palavraEscondida);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesafioPalindromo/DesafioPalindromo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPalindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra: ");
+            string palavra = Console.ReadLine().ToUpper();
+
+            string palavraPalindromo = "";
+
+            for (int i = palavra.Length - 1; i >= 0; i--)
+            {
+
+                palavraPalindromo += palavra[i];
+
+            }
+
+            string resultado = palavraPalindromo == palavra ? "A string " + palavra + " é um palíndromo" : "A string " + palavra + " não é um palíndromo";
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
